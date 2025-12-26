aec25ec87177f27c7f75e3491e4c73b9bf74197a
Peter Arato <it.arato@gmail.com>
2025-12-26 10:16:04 -0500

    Add clap.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,34 @@
-#![allow(unused_variables)]
-use std::env;
+use clap::Parser;
 use std::fs;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct ProgramArgs {
+    command: String,
+    filename: String,
+}
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        eprintln!("Usage: {} tokenize <filename>", args[0]);
-        return;
-    }
+    let program_args = ProgramArgs::parse();
 
-    let command = &args[1];
-    let filename = &args[2];
-
-    match command.as_str() {
+    match program_args.command.as_str() {
         "tokenize" => {
             // You can use print statements as follows for debugging, they'll be visible when running tests.
             eprintln!("Logs from your program will appear here!");
 
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                eprintln!("Failed to read file {}", filename);
+            let file_contents = fs::read_to_string(&program_args.filename).unwrap_or_else(|_| {
+                eprintln!("Failed to read file {}", program_args.filename);
                 String::new()
             });
 
-            // TODO: Uncomment the code below to pass the first stage
-            // if !file_contents.is_empty() {
-            //     panic!("Scanner not implemented");
-            // } else {
-            //     println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
-            // }
+            if !file_contents.is_empty() {
+                panic!("Scanner not implemented");
+            } else {
+                println!("EOF  null");
+            }
         }
-        _ => {
-            eprintln!("Unknown command: {}", command);
+        other => {
+            eprintln!("Unknown command: {}", other);
         }
     }
 }
